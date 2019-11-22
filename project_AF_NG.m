81de7362c911553c2ea38161c869d181214fcbfa
aaf4489 <aaf4489@cs.rit.edu>
2019-11-22 16:02:48 -0500

    Changed variable names to be more understandable, added comments, and added harris corners

--- project_AF_NG.m
+++ project_AF_NG.m
@@ -12,35 +12,47 @@ function main()
         im = imread( file_names(file_idx).name );
         im_gray = rgb2gray(im);
         
+        % Define filters
         filt_dx = fspecial('average');
         filt_dy = filt_dx.';
         filt2_dx = fspecial('log');
         filt2_dy = filt2_dx.';
+        filt3_dx = fspecial('gaussian');
+        filt3_dy = filt3_dx.';
         
+        % Apply the average filter to the horizontal and vertical
+        % directions and then combine the results
         im_filt_x = imfilter( im_gray, filt_dx );
         im_filt_y = imfilter( im_gray, filt_dy );
-
         im_filt = im_filt_x + im_filt_y;
         
+        % Apply a Laplacian of Gaussian filter to the averaged image in the
+        % vertical and horizontal directions and then combine the results
         im_filt_x2 = imfilter( im_filt, filt2_dx );
         im_filt_y2 = imfilter( im_filt, filt2_dy );
-
         im_filt2 = im_filt_x2 + im_filt_y2;
         
-        im_test = edge(im_filt2, 'canny', [0.05 0.3]);
+        
+        im_canny = edge(im_filt2, 'canny', [0.05 0.3]);
         
         % Combine edges of the three images to reduce error and get more of
         % the edges that other images may miss. 
         if(cnt == 0)
-            im_comb = im_test;
+            im_comb = im_canny;
         elseif(cnt == 3)
+            
+            corners = detectHarrisFeatures(im_comb);
+            
             figure
             imshow(im_comb)
+            
+            hold on;
+            plot(corners.selectStrongest(50));
   
-            im_comb = im_test;
+            im_comb = im_canny;
             cnt = 0;
         else
-            im_comb = im_comb + im_test;
+            im_comb = im_comb + im_canny;
         end
         
         cnt = cnt + 1;
